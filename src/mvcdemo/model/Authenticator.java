c36140df9121385f012f75e14decc66091d84ed0
Ryan Callaway <callaway.r@gmail.com>
2016-07-13 12:24:45 -0700

    Multiple users HashMap sign-in

--- src/mvcdemo/model/Authenticator.java
+++ src/mvcdemo/model/Authenticator.java
@@ -1,10 +1,20 @@
 package mvcdemo.model;
 
+import java.util.HashMap;
+
 public class Authenticator {
+
+	private static HashMap<String, String> users;
+	static
+	{
+		users = new HashMap<String, String>();
+		users.put("ryan", "password");
+		users.put("sara", "password");
+	}
 	
 	public String authenticate(String username, String password) {
-		if (("ryan".equalsIgnoreCase(username))
-				&& ("password".equals(password))) {
+		if ((users.containsKey(username))
+				&& (users.get(username).equals(password))) {
 			return "success";
 		} else {
 			return "failure";
